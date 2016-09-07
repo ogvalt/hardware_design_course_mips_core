6892f09fec8b3c16b1cb212e519bc400b65e6941
ogvalt <ogvalt@gmail.com>
2016-09-07 21:18:51 +0300

    Impove rom.v code and map module's instance ports in fetch.v

--- unpipeline/fetch.v
+++ unpipeline/fetch.v
@@ -1,19 +1,31 @@
 module fetch(i_clk, i_rst_n, i_execute, i_pcsrc, o_fetch_pc, o_fetch_instr);
   
-  input wire i_clk;
-  input wire i_rst_n; //active zero level
-  input wire i_pcsrc; //mux control
+  input i_clk;
+  input i_rst_n; //active zero level
+  input i_pcsrc; //mux control
   
-  input wire  [31:0] i_execute; // out of nextPC
+  input  [31:0] i_execute; // out of nextPC
   
-  output wire [31:0] o_fetch_pc; //input for nextPC and PC
-  output wire [31:0] o_fetch_instr; //instraction fetch
+  output [31:0] o_fetch_pc; //input for nextPC and PC
+  output [31:0] o_fetch_instr; //instraction fetch
   
   wire [31:0] i_pc; //input previously addr
   
-  pc PC(i_clk, i_rst_n, i_pc, o_fetch_pc); 
-  mux2in1 PCSOURCE (o_fetch_pc, i_execute, i_pcsrc, i_pc);// mux source of addr of pc
-  rom ROM(o_fetch_pc, o_fetch_instr);
+  pc PC  ( .i_clk   (i_clk), 
+           .i_rst_n (i_rst_n), 
+           .i_pc    (i_pc), 
+           .o_pc    (o_fetch_pc)
+          );
+
+  mux2in1 PCSOURCE ( .i_dat0    (o_fetch_pc), 
+                     .i_dat1    (i_execute), 
+                     .i_control (i_pcsrc), 
+                     .o_dat     (i_pc)
+                    );// mux source of addr of pc
+
+  rom ROM   ( .i_addr(o_fetch_pc), 
+              .o_data(o_fetch_instr)
+            );
   
 
 endmodule
--- unpipeline/rom.v
+++ unpipeline/rom.v
@@ -1,13 +1,13 @@
-module rom
-#(parameter DATA_WIDTH=32, parameter ADDR_WIDTH=128)
-(
-	input [(ADDR_WIDTH-1):0] i_addr,
-	output [(DATA_WIDTH-1):0] o_data
-);
+module rom ( i_addr, o_data );
 
-reg [DATA_WIDTH-1:0] memory [0:ADDR_WIDTH-1];
+	parameter DATA_WIDTH=32; 
+	parameter ADDR_WIDTH=128;
 
+	input [(ADDR_WIDTH-1):0] i_addr;
+	output [(DATA_WIDTH-1):0] o_data;
 
-assign o_data = memory[i_addr];
+	reg [DATA_WIDTH-1:0] memory [0:ADDR_WIDTH-1];
+
+	assign o_data = memory[i_addr];
 
 endmodule
