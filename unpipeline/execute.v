86349f015125ef4c2e2af98bcb0d309d6adfd7e4
ogvalt <ogvalt@gmail.com>
2016-09-07 22:02:26 +0300

    Map instance's ports in execute.v

--- unpipeline/execute.v
+++ unpipeline/execute.v
@@ -3,30 +3,59 @@ module execute(i_pc, i_imm, i_op1, i_op2,
                i_beq, i_bne, o_op2, 
                o_ALUres, o_nextPC, o_pcsrc);
   
-  input wire  [31:0] i_pc; //address from pc
-  input wire  [25:0] i_imm; //immidiate constant input
-  input wire  [31:0] i_op1, i_op2; //operandrs input
-  input wire         i_ALUSrc;
-  input wire  [ 1:0] i_ALUop;
-  input wire         i_jump, i_beq, i_bne; //condition control
-  input wire         i_extOp; // extender control
+  input  [31:0] i_pc; //address from pc
+  input  [25:0] i_imm; //immidiate constant input
+  input  [31:0] i_op1, i_op2; //operandrs input
+  input         i_ALUSrc;
+  input  [ 1:0] i_ALUop;
+  input         i_jump, i_beq, i_bne; //condition control
+  input         i_extOp; // extender control
   
-  output wire [31:0] o_op2;
-  output wire [31:0] o_ALUres;
-  output wire [31:0] o_nextPC;
-  output wire        o_pcsrc;
+  output [31:0] o_op2;
+  output [31:0] o_ALUres;
+  output [31:0] o_nextPC;
+  output        o_pcsrc;
   
   wire               zerof; // zero flag - alures=0
   wire        [31:0] extended; //value after extender
   wire        [31:0] aluOp2; //second alu operand
   wire        [ 3:0] ALUCtrl; //aclu control code
   
-  signExtend EXTENDER(i_imm[15:0], i_extOp, extended);
-  mux2in1 ALUSOURCE(i_op2, extended, i_ALUSrc, aluOp2);
-  alu ALU(i_op1, aluOp2, ALUCtrl, o_ALUres, zerof);
-  aluControl ALUCONTROL(i_ALUop, i_imm[5:0], ALUCtrl);
-  nextPC NEXTPC(i_pc, i_imm, i_jump, i_beq, i_bne, 
-                zerof, o_nextPC, o_pcsrc);
+  signExtend EXTENDER( .i_data    (i_imm[15:0]), 
+                       .i_control (i_extOp), 
+                       .o_data    (extended)
+                      );
+
+  mux2in1 ALUSOURCE ( .i_data0    (i_op2), 
+                      .i_data0    (extended), 
+                      .i_control  (i_ALUSrc), 
+                      .o_dat      (aluOp2)
+                    );
+
+  alu ALU ( 
+            .i_op1  (i_op1), 
+            .i_op2  (aluOp2), 
+            .i_control(ALUCtrl), 
+            .o_result(o_ALUres), 
+            .o_zf(zerof)
+          );
+
+  aluControl ALUCONTROL(
+                        .i_aluOp(i_ALUop), 
+                        .i_func(i_imm[5:0]), 
+                        .o_aluControl(ALUCtrl)
+                        );
+  nextPC NEXTPC (
+                  .i_pc(i_pc), 
+                  .i_imm(i_imm), 
+                  .i_jump(i_jump), 
+                  .i_beq(i_beq), 
+                  .i_bne(i_bne), 
+                  .i_zerof(zerof), 
+                  .o_nextPC(o_nextPC), 
+                  .o_pcsrc(o_pcsrc)
+                );
                 
   assign o_op2 = i_op2;
+  
 endmodule
